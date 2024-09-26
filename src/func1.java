cc9d4cdcecf636e8dfeb07bb6bd6bc17fb726b80
Ostap Prodanyuk <OstapoKapo@gmail.com>
2024-09-26 16:00:57 +0300

    add classes

---
+++ src/func1.java
@@ -0,0 +1,5 @@
+public class func1 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/func2.java
+++ src/func2.java
@@ -1,7 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class func2 {
     public static void main(String[] args) {
-
+System.out.println("Hello");
     }
 }
