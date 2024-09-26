570809d7569a91d94bfd4fabcf9aed0c977c038a
Ostap Prodanyuk <OstapoKapo@gmail.com>
2024-09-26 16:21:40 +0300

    change func 2

--- src/func2.java
+++ src/func2.java
@@ -2,6 +2,35 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class func2 {
     public static void main(String[] args) {
-System.out.println("Hello");
+        printResults(0, 0);
+        printResults(0, 1);
+        printResults(1, 0);
+        printResults(1, 1);
+        printResults(2, 1);
+        printResults(1, 2);
+        printResults(2, -10);
+        printResults(20, 20);
+        printResults(21, 21);
+        printResults(1, 10);
+        printResults(-1, 10);
+        printResults(0, Double.MAX_VALUE);
+        printResults(1, Double.MAX_VALUE);
+        printResults(1, Double.NaN);
+    }
+
+    public static double axx(double a, double x) {
+        if (a < 0 || a > 20) {
+            throw new IllegalArgumentException("param a = " + a);
+        }
+        return a * x * x;
+    }
+
+    static void printResults(double a, double x) {
+        System.out.print("a:" + a + " x:" + x + " result:");
+        try {
+            System.out.println(axx(a, x));
+        } catch (IllegalArgumentException e) {
+            System.out.println("EXCEPTION! " + e.getMessage());
+        }
     }
 }
