0c3dca59d2c350d4696ebaedb77f47ae21e5e621
Ostap Prodanyuk <OstapoKapo@gmail.com>
2024-09-27 03:30:50 +0300

    changed func 1

--- src/func1.java
+++ src/func1.java
@@ -1,31 +1,31 @@
 public class func1 {
     public static void main(String[] args) {
-        result(0, 0);
-        result(0, 1);
-        result(1, 0);
-        result(2, 1);
-        result(1, 2);
-        result(2, 15);
+        printResults(0, 0);
+        printResults(0, 1);
+        printResults(1, 0);
+        printResults(2, 1);
+        printResults(4, -2);
+        printResults(2, 15);
 
 
     }
 
-    public static double sum(double k, double z) {
+    public static double executionEq(double k, double z) {
         double sum = 0;
-        if (k > 15 || z <= 0) {
+        if (k > 15) {
             throw new IllegalArgumentException("param k = " + k);
 
         }
         for (int i = 1; i < k; i++) {
-             sum += (1 / z * i) + Math.tan(k / i);
-            }
+             sum += (1/z*i) + Math.tan(k / i);
+        }
         return sum;
     }
 
-    static void result(double k, double z) {
-        System.out.print("z=" + z + "k=" + k + "result: ");
+    static void printResults(double k, double z) {
+        System.out.print("z=" + z + " k=" + k + " result: ");
         try {
-            System.out.println(sum(k, z));
+            System.out.println(executionEq(k, z));
         } catch (IllegalArgumentException e) {
             System.out.println("EXCEPTION! " + e.getMessage());
 
